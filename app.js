a49a998cff95e7b900828faaeac68aa414748a67
lizzaaabb <lbogveradze12@gmail.com>
2024-08-23 15:59:20 +0400

    phone

---
+++ app.js
@@ -0,0 +1,4 @@
+const home = document.getElementById("home")
+const project = document.getElementById("project")
+const info = document.getElementById("info")
+const contact = document.getElementById("contact")
